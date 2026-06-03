2ab7102ebaf583a3d83ff1113463b38749677d34
Alif <labibul.ahsan.alif@g.bracu.ac.bd>
2026-06-03 15:40:51 +0600

    learn: learn about props

---
+++ react-tutorial/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+    return (
+        <>
+            <h1>Animals:</h1>
+            <ul>
+                <li>Cow</li>
+                <li>Tiger</li>
+                <li>Cat</li>
+                <li>Dog</li>
+            </ul>
+        
+        
+        
+        </>
+    );
+}
+export default App;
--- react-tutorial/src/main.jsx
+++ react-tutorial/src/main.jsx
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Greeting, { Details } from './Greeting'
 
+import App from './App'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <App/>
     <Greeting />
     <Details />
-  </StrictMode>,
+  </StrictMode>
 )
