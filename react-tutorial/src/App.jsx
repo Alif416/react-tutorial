3a986d6c59cf137901c3f08c8b004896eca3cabe
Alif <labibul.ahsan.alif@g.bracu.ac.bd>
2026-06-04 12:57:50 +0600

    fix: tried to fixed some bug by my own in sample code snippet

--- react-tutorial/src/App.jsx
+++ react-tutorial/src/App.jsx
@@ -1,17 +1,30 @@
+function List({ animals }) {
+  return (
+    <div>
+      {animals.length > 0 ? (
+        <ul>
+          {animals.map((animal) => (
+            <li key={animal}>{animal}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>There is no animal exist</p>
+      )}
+    </div>
+  );
+}
+
+
+
+
 function App() {
+    const animals = ['Cow', 'Cat','Dog'];
+
     return (
-        <>
-            <h1>Animals:</h1>
-            <ul>
-                <li>Cow</li>
-                <li>Tiger</li>
-                <li>Cat</li>
-                <li>Dog</li>
-            </ul>
-        
-        
-        
-        </>
+        <div>
+            <h1>Animals: </h1>
+            <List animals={animals} />
+        </div>
     );
 }
 export default App;
