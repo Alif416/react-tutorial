ab1374be22ccfc17b77c5241ea414f9fe035a4c6
Alif <labibul.ahsan.alif@g.bracu.ac.bd>
2026-06-08 12:48:34 +0600

    Learn: pass data through app compoent to Student component

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,17 +1,13 @@
 
 import "./App.css";
+
 import Student from "./Student";
 
 
 export default function App() {
   return (
     <div>
-      <Student name="Labibul Ahsan Alif" age={24} isStudent={true}
-      />
-      <Student name="John Doe" age={30} isStudent={false}
-      />
-      <Student name="Jane Smith" age={22} isStudent={true}
-      />
+      <Student name='Alif' age={25} isStudent={true} />
     </div>
   )
 }
--- my-react-app/src/Student.jsx
+++ my-react-app/src/Student.jsx
@@ -1,11 +1,14 @@
+function Student(props) {
+    return ( 
+        <div>
+            <p>Name: {props.name}</p>
+            <p>Age: {props.age}</p>
+            <p>Student: {props.isStudent? 'Yes':'No' }</p>
+            
+        </div>
 
-export default function Student(props){
-    return (
-    <div className="student">
-        <p>Name: {props.name} </p>
-        <p>Age: {props.age}</p>
-        <p>Student: { props.isStudent?"Yes":"No"}</p>
-
-    </div>
-    )
+        
+    );
 }
+
+export default Student;
