712c93372999721351557063ffd96a33dcd24d24
Alif <labibul.ahsan.alif@g.bracu.ac.bd>
2026-06-19 16:06:15 +0600

    worked with mini project with react props

---
+++ css/src/components/Button.jsx
@@ -0,0 +1,11 @@
+
+import '../styles/Button.css'
+const Button = () => {
+  return (
+      <div className='btn'>
+          <button>This is a button</button>
+    </div>
+  )
+}
+
+export default Button
