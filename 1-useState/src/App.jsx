a38feb54bf546055b8446ab052fea2c82ee09619
Alif <labibul.ahsan.alif@g.bracu.ac.bd>
2026-06-21 20:54:53 +0600

    add decrease button and prevent counter from going below 0

--- 1-useState/src/App.jsx
+++ 1-useState/src/App.jsx
@@ -1,18 +1,28 @@
 import { useState } from "react"
-
 const App = () => {
-  const [num, setName] = useState(0)
-  function onclicked() {
-    if (num >= 10) return
-    setName(num + 1)
+  const [num, setNum] = useState(0)
+  function increase() {
+    setNum(num+1)
+    
+  }
+  function decrease() {
+    if (num > 0) {
+      return setNum(num-1)
+    }
     
   }
   return (
+
     <div>
-      <h1>The Number is  { num}</h1>
-      <button onClick={onclicked}>Increase The Number</button>
+      <h1 className="num">{ num}</h1>
+      <div className="btn">
+      <button className="btn1" onClick={increase}>Increase</button>
+      <button className="btn2" onClick={decrease}>Decrease</button>
+      </div>
+      
+      
+
     </div>
   )
 }
-
 export default App
