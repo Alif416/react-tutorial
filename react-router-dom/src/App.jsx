aa433fd3a2454fb78c1d330a401e3448448fc58d
Alif <labibul.ahsan.alif@g.bracu.ac.bd>
2026-06-30 11:55:30 +0600

    Learn BrowserRouter and how to create Routes

--- react-router-dom/src/App.jsx
+++ react-router-dom/src/App.jsx
@@ -1,8 +1,16 @@
-
-
+import About from './pages/About'
+import {Route,Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
 const App = () => {
   return (
     <div>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/contact' element={<Contact/>}></Route>
+
+      </Routes>
       
     </div>
   )
